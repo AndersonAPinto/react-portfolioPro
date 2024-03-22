074abae0d55bb7d342449059a385e3dffb08e78c
AndersonAPinto <andersonaugustopinto@gmail.com>
2024-03-21 23:30:10 -0300

    update in screens

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,37 +1,32 @@
 import React from "react";
 import { HiArrowDownRight } from "react-icons/hi2";
-import { SlSocialGithub, SlSocialFacebook, SlSocialLinkedin, SlSocialInstagram } from "react-icons/sl";
-import { FaWhatsapp } from "react-icons/fa";
-import { BiMailSend } from "react-icons/bi";
-import Banner from '../../imgs/banner.jpg'
-
+import { FaWhatsapp, FaLinkedinIn, FaFacebook, FaGithub, FaInstagram, FaRegEnvelope } from "react-icons/fa";
 import './contact.css';
+import './contactmobile.css';
 
 function Contact() {
     return (
         <div className="container__Contact">
-            <div className="frame">
-                {/*<img src={Banner} style={{width:'27vw', height:'75vh', borderRadius:'10px'}}/>*/}
-            </div>
             <div className="title_contact">
                 <h1> <HiArrowDownRight /> Contato</h1>
-            </div>
-            <div className="div_title">               
-                <div className="icons_contact">
-                    <h2 style={{ fontSize: '35px', marginBottom:'5px',textAlign:'center' }}>Redes Sociais e Envio de Mensagens</h2>
-                    <h6 style={{ marginBottom: '5px', color: '#C68107',fontSize:'1em', textAlign:'center' }}>Conecte-se Conosco com Apenas um Clique</h6>
-                    <p style={{ color: '#b1b1b1', fontSize:'1.2em' }}>
-                        Valorizo cada oportunidade de interagir com você. Para tornar sua experiência comigo ainda mais direta e conveniente, disponibilizo ícones de acesso rápido abaixo. Cada ícone é uma porta para minhas principais plataformas de comunicação e redes sociais, permitindo que você escolha o canal preferencial de contato com facilidade e rapidez.
-                        Seja para fazer perguntas, solicitar suporte, ou compartilhar suas impressões, estamos a apenas um clique de distância. Estarei comprometido em fornecer respostas e soluções ágeis e atenciosas.
-                        Estou animado para ouvir o que você tem a dizer.</p>
-                </div>
-                <div className="div_icons">
-                    <a href="https://www.linkedin.com/in/andersonapinto/" target="_blank" rel="noopener noreferrer"><SlSocialLinkedin /></a>
-                    <a href="https://github.com/AndersonAPinto" target="_blank" rel="noopener noreferrer"><SlSocialGithub /></a>
-                    <a href="https://www.facebook.com/andersonaspinto/" target="_blank" rel="noopener noreferrer"><SlSocialFacebook /></a>
-                    <a href="https://www.instagram.com/andersonaugusto.pinto/" target="_blank" rel="noopener noreferrer"><SlSocialInstagram /></a>
-                    <a href="https://wa.me/5551997802755" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
-                    <a href="mailto:andersonaugustopinto@gmail.com" target="_blank" rel="noopener noreferrer"><BiMailSend style={{ fontSize: '54px' }} /></a>     
+            </div><div className="frame">
+                <div className="div_title">
+                    <div className="icons_contact">
+                        <h2 style={{ fontSize: '2em', marginBottom: '5px', textAlign: 'center' }}>Redes Sociais e Envio de Mensagens</h2>
+                        <h6 style={{ marginBottom: '5px', color: '#C68107', fontSize: '1em', textAlign: 'center' }}>Conecte-se Conosco com Apenas um Clique</h6>
+                        <p style={{ color: '#b1b1b1', fontSize: '1.2em' }}>
+                            Valorizo cada oportunidade de interagir com você. Para tornar sua experiência comigo ainda mais direta e conveniente, disponibilizo ícones de acesso rápido abaixo. Cada ícone é uma porta para minhas principais plataformas de comunicação e redes sociais, permitindo que você escolha o canal preferencial de contato com facilidade e rapidez.
+                            Seja para fazer perguntas, solicitar suporte, ou compartilhar suas impressões, estamos a apenas um clique de distância. Estarei comprometido em fornecer respostas e soluções ágeis e atenciosas.
+                            Estou animado para ouvir o que você tem a dizer.</p>
+                    </div>
+                    <div className="div_icons">
+                        <a href="https://www.linkedin.com/in/andersonapinto/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
+                        <a href="https://github.com/AndersonAPinto" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                        <a href="https://www.facebook.com/andersonaspinto/" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+                        <a href="https://www.instagram.com/andersonaugusto.pinto/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+                        <a href="https://wa.me/5551997802755" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
+                        <a href="mailto:andersonaugustopinto@gmail.com" target="_blank" rel="noopener noreferrer"><FaRegEnvelope /></a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Pages/Experience.jsx
+++ src/components/Pages/Experience.jsx
@@ -6,6 +6,7 @@ import WebSiteCOnsEletr from '../../imgs/WebSiteConsumptionEletricCar.png';
 import App_1 from '../../imgs/App_1.png'
 
 import './experience.css';
+import './experiencemobile.css';
 
 function Experience() {
     return (
@@ -14,49 +15,48 @@ function Experience() {
                 <h1><HiArrowDownRight />Experiências</h1>
             </div>
             <hr style={{ margin: '25px', width: '70vw' }} />
-            <div>
-                <div className="imgs_site" >
-                    <p style={{ fontSize: '1.3em', marginLeft:'5px' }}>
-                        <h6 style={{ color: '#C68107' }}>Controle de Ponto</h6>
-                        Aplicativo desenvolvido baseado em um problema real. para controlar a carga horária e não ultrapassa-la, esse app identifica os dias utéis, feriados nacionais e regionais e calcula qual o período necessário para que a carga horária permaneça correta.
-                        App em Desenvolvimento de backend, construido com Figma, e desenvolvido com React Native, trará a solução adequada para o problema, futuramente estará presente na Google Play com o mesmo nome do título. 
-                    </p>
-                    <img src={App_1} style={{ width: '25%', height: '450px', marginLeft: '10vw', borderRadius: '10px' }} />
-                </div>
+            <div className="imgs_site" >
+                <p style={{ fontSize: '1.3em', marginLeft: '5px' }}>
+                    <h6 style={{ color: '#C68107' }}>Controle de Ponto</h6>
+                    Aplicativo desenvolvido baseado em um problema real. para controlar a carga horária e não ultrapassa-la, esse app identifica os dias utéis, feriados nacionais e regionais e calcula qual o período necessário para que a carga horária permaneça correta.
+                    App em Desenvolvimento de backend, construido com Figma, e desenvolvido com React Native, trará a solução adequada para o problema, futuramente estará presente na Google Play com o mesmo nome do título.
+                </p>
+                <img src={App_1} style={{ width: '25%', height: '25em', marginLeft: '10vw', borderRadius: '10px' }} />
             </div>
+
             <hr style={{ margin: '25px', width: '70vw' }} />
-            <div>
-                <div className="imgs_site" >
-                    <img src={SiteGadj} style={{ width: '50%', height: '250px', marginRight: '15px', borderRadius: '10px' }} />
-                    <p style={{ fontSize: '1.3em', marginRight:'1vw' }}>
-                        <h6 style={{ color: '#C68107' }}>GADJ Ferramentas</h6>
-                        O site é uma plataforma de e-commerce especializada na venda de ferramentas e produtos industriais, criada para atender às necessidades de profissionais e empresas do setor. Com um catálogo diversificado, equipamentos sofisticados para grandes operações industriais. Destaco a facilidade de navegação do site, proporcionando uma busca ágil e satisfatória por soluções industriais completas.
-                    </p>
-                </div>
+
+            <div className="imgs_site" >
+                <img src={SiteGadj} />
+                <p style={{ fontSize: '1.3em', marginRight: '1vw' }}>
+                    <h6 style={{ color: '#C68107' }}>GADJ Ferramentas</h6>
+                    O site é uma plataforma de e-commerce especializada na venda de ferramentas e produtos industriais, criada para atender às necessidades de profissionais e empresas do setor. Com um catálogo diversificado, equipamentos sofisticados para grandes operações industriais. Destaco a facilidade de navegação do site, proporcionando uma busca ágil e satisfatória por soluções industriais completas.
+                </p>
             </div>
+
             <hr style={{ margin: '25px', width: '70vw' }} />
-            <div>
-                <div className="imgs_site" >
-                    <p style={{ fontSize: '1.3em', marginLeft:'1vw' }}>
-                        <h6 style={{ color: '#C68107', marginTop:'4px' }}>Teste de Inglês</h6>
-                        Site oferece uma ferramenta interativa para avaliar a proficiência em inglês de usuários através de um teste composto por 20 perguntas cuidadosamente elaboradas, baseadas no Quadro Europeu Comum de Referência para Línguas (CEFR). Ao concluir o teste, os participantes recebem uma avaliação precisa de seu nível de inglês, variando de A1 (iniciante) a C2 (Avançado), permitindo-lhes entender claramente suas competências linguísticas atuais. 
-                        <a  href="https://testedeingles.app.br/" target="_blank" rel="noopener noreferrer" style={{color:'#ff6b08'}}> Confira</a>.
-                    </p>
-                    <img src={WebSite_Englistest} style={{ width: '50%', height: '250px', marginLeft: '15px', borderRadius: '10px' }} />
-                </div>
+
+            <div className="imgs_site" >
+                <p style={{ fontSize: '1.3em', marginLeft: '1vw' }}>
+                    <h6 style={{ color: '#C68107', marginTop: '4px' }}>Teste de Inglês</h6>
+                    Site oferece uma ferramenta interativa para avaliar a proficiência em inglês de usuários através de um teste composto por 20 perguntas cuidadosamente elaboradas, baseadas no Quadro Europeu Comum de Referência para Línguas (CEFR). Ao concluir o teste, os participantes recebem uma avaliação precisa de seu nível de inglês, variando de A1 (iniciante) a C2 (Avançado), permitindo-lhes entender claramente suas competências linguísticas atuais.
+                    <a href="https://testedeingles.app.br/" target="_blank" rel="noopener noreferrer" style={{ color: '#ff6b08' }}> Confira</a>.
+                </p>
+                <img src={WebSite_Englistest} style={{ marginLeft: '15px' }} />
             </div>
+
             <hr style={{ margin: '25px', width: '70vw' }} />
-            <div >
-                <div className="imgs_site" >
-                    <img src={WebSiteCOnsEletr} style={{ width: '50%', height: '250px', marginRight: '15px', borderRadius: '10px' }} />
-                    <p style={{ fontSize: '1.3em', marginRight:'1vw' }}>
-                        <h6 style={{ color: '#C68107' }}>Consumo de carros Elétricos</h6>
-                        O site oferece uma ferramenta inovadora e essencial para proprietários de veículos elétricos, simplificando o cálculo do custo de uma recarga completa da bateria. Ao acessar a plataforma, os usuários podem facilmente selecionar sua rede de energia local e inserir a potência específica da bateria de seu veículo. Com base nessas informações, o sistema realiza automaticamente o cálculo, fornecendo uma estimativa precisa do custo de recarga.
-                    </p>
-                </div>
+
+            <div className="imgs_site" >
+                <img src={WebSiteCOnsEletr} />
+                <p style={{ fontSize: '1.3em', marginRight: '1vw' }}>
+                    <h6 style={{ color: '#C68107' }}>Consumo de carros Elétricos</h6>
+                    O site oferece uma ferramenta inovadora e essencial para proprietários de veículos elétricos, simplificando o cálculo do custo de uma recarga completa da bateria. Ao acessar a plataforma, os usuários podem facilmente selecionar sua rede de energia local e inserir a potência específica da bateria de seu veículo. Com base nessas informações, o sistema realiza automaticamente o cálculo, fornecendo uma estimativa precisa do custo de recarga.
+                </p>
             </div>
+
             <hr style={{ margin: '25px', width: '70vw' }} />
-            
+
         </div>
     )
 }
